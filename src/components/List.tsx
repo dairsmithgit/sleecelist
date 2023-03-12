4a6e7578ea706968de412cdd09b210d087a4e45a
Dair Smith <dair.code@gmail.com>
2023-03-11 22:35:32 -0500

    start search, reformat list

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,25 +1,30 @@
-import { Box, Flex, Text } from "@chakra-ui/react";
+import { Box, Flex, Text, Button } from "@chakra-ui/react";
 import { useAtom } from "jotai";
 
 import { animesListAtom } from "../store/store";
 
 function List() {
   const [animeList, setAnimeList] = useAtom(animesListAtom);
+  console.log(animeList);
 
-  return (
-    <Box m={2}>
-      {animeList ? (
-        animeList.map((anime) => (
+  if (animeList.length > 0) {
+    return (
+      <Box m={2}>
+        {animeList.map((anime) => (
           <Flex gap={2}>
             <Text fontWeight="bold">{anime.title?.romaji}</Text>
             <Text>({anime.title?.native})</Text>
           </Flex>
-        ))
-      ) : (
+        ))}
+      </Box>
+    );
+  } else {
+    return (
+      <Box m={2}>
         <Text fontWeight="bold">No animes in this list to display...</Text>
-      )}
-    </Box>
-  );
+      </Box>
+    );
+  }
 }
 
 export default List;
---
+++ src/components/Search.tsx
@@ -0,0 +1,10 @@
+import { Box, Text, Input } from "@chakra-ui/react";
+
+function Search() {
+  return (
+    <Box>
+      <Text>Search Anime</Text>
+      <Input placeholder="Name" />
+    </Box>
+  );
+}
