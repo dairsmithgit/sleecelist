e74fce3b0a42785b2039794499c8710892b59075
Dair Smith <dair.code@gmail.com>
2023-03-14 23:32:53 -0400

    anime list component

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,5 +1,6 @@
-import { Box, Flex, Text } from "@chakra-ui/react";
+import { Box, Flex, Heading, Text, IconButton } from "@chakra-ui/react";
 import { useAtom } from "jotai";
+import { FiCopy } from "react-icons/fi";
 
 import { animesListAtom } from "../store/store";
 
@@ -9,18 +10,31 @@ function List() {
 
   if (animeList.length > 0) {
     return (
-      <Box m={2}>
-        {animeList.map((anime) => (
-          <Flex gap={2}>
-            <Text fontWeight="bold">{anime.title?.romaji}</Text>
-            <Text>({anime.title?.native})</Text>
-          </Flex>
-        ))}
-      </Box>
+      <Flex flexDirection="column" mt={1}>
+        <Flex gap={2} justifyContent="center" alignItems="center">
+          <Heading>Your Anime List</Heading>
+          <IconButton
+            aria-label="copy to clipboard"
+            icon={<FiCopy />}
+            variant="menu"
+            onClick={() => {
+              navigator.clipboard.writeText("hello"); // make the text the contents of state or the contents of the div
+            }}
+          />
+        </Flex>
+        <Box mx="auto" my={2} w="50%" border="1px">
+          {animeList.map((anime) => (
+            <Flex gap={2} p={1} justifyContent="center">
+              <Text fontWeight="bold">{anime.title?.romaji}</Text>
+              <Text>({anime.title?.native})</Text>
+            </Flex>
+          ))}
+        </Box>
+      </Flex>
     );
   } else {
     return (
-      <Box m={2}>
+      <Box m={2} textAlign="center">
         <Text fontWeight="bold">No animes in this list to display...</Text>
       </Box>
     );
